02ba918735f8000aea91775eaae901c41baa376f
Omar Cerqueira <omarcerqueira12@gmail.com>
2024-09-19 11:27:22 -0300

    adicionados app-shop-side-bar e scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  // // Método que retorna a quantidade de produtos
+  // getQtdeProdutos(): number {
+  //   return this.produtos.length;  // Retorna o número de produtos
+  // }
 }
