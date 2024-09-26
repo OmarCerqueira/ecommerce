4865c51c371b2f1da69627cab3a2a0f9fb43bb70
Omar Cerqueira <omarcerqueira12@gmail.com>
2024-09-26 19:49:37 -0300

    diretivas ngIf/Else, ngStyle, ngClass e ngFor

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { getActiveConsumer } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.css'
 })
@@ -13,6 +15,7 @@ export class NewsletterComponent implements OnInit {
   permiteNewsletter = false;
   textoNewsletter = '... e obtenha os melhores descontos';
   email: string = 'omar.duleu@gmail.com';
+  emailEnviado = false;
 
   ngOnInit(): void {
     setTimeout((): void => {
@@ -22,9 +25,18 @@ export class NewsletterComponent implements OnInit {
 
   mudarNewsletter(){
     this.textoNewsletter = 'Texto modificado';
+    this.emailEnviado = true;
   }
 
   digitarEmail(event: Event) {
     this.email = (<HTMLInputElement>event.target).value;
   }
+
+  // getCor() {
+  //   if (this.emailEnviado){
+  //     return 'green';
+  //   } else {
+  //     return 'orange';
+  //   }
+  // }
 }
--- src/app/product/product-item/product-item.component.ts
+++ src/app/product/product-item/product-item.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
 export class ProductItemComponent {
+
+  // produtos = ['TV', 'Radio', 'Fone', 'Microondas', 'Fogão'];
+
+  produtos = [
+    { nome: 'Computador', preco: 458.56 },
+    { nome: 'Fogão', preco: 487.52 },
+    { nome: 'Geladeira', preco: 521.25 },
+    { nome: 'Notebook', preco: 215.23 },
+    { nome: 'Cama', preco: 459.69 }
+  ];
+
+
 
 }
